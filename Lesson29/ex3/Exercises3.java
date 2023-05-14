9c3711aa9056579e6381a068fa937d37f97f9696
Than Trieu <34931034+thantrieu@users.noreply.github.com>
2023-05-14 15:17:33 +0700

    Update Exercises3.java
    V2023.05.14

--- Lesson29/ex3/Exercises3.java
+++ Lesson29/ex3/Exercises3.java
@@ -1,34 +1,59 @@
+import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Exercises3 {
     public static void main(String[] args) {
-        // giả định nhập chuẩn thông tin:
-        BankAccount acc = new BankAccount();
+        // giả định ta có 2 tài khoản:
+        BankAccount acc = new BankAccount("0021001", "LE CONG TUAN", "Debit", 125_000_000);
+        BankAccount acc2 = new BankAccount("0021002", "MAI DUC TRUNG", "Debit", 15_000_000);
         var input = new Scanner(System.in);
-        System.out.println("Nhập số tài khoản của bạn: ");
-        var accNum = input.nextLine();
-        acc.setAccNumber(accNum);
-        System.out.println("Nhập tên tài khoản: ");
-        var owner = input.nextLine();
-        acc.setOwner(owner);
-        System.out.println("Nhập số tiền muốn gửi vào TK: ");
-        acc.deposit(input.nextLong());
+        showAccInfo(acc);
 
-        // lấy thông tin tài khoản thông qua các getter:
-        System.out.println("Số TK: " + acc.getAccNumber());
-        System.out.println("Tên TK: " + acc.getOwner());
-
-        // thực hiện các thanh toán khác:
+        // thực hiện các thanh toán:
         System.out.println("Nhập số tiền muốn rút: ");
         var amount = input.nextLong();
         if (acc.withdraw(amount)) {
             System.out.println("Rút thành công");
             acc.checkAccBallance();
         } else {
-            System.out.println("Số dư của bạn không đủ thực hiện giao dịch này");
+            System.out.println("==> LỖI: Số dư của bạn không đủ.");
         }
-        System.out.println("Bạn có muốn thực hiện giao dịch khác không? ");
-        // var dec = input.nextLine(); // nhập có/không
-        // thêm các hành động khác ở đây...
+
+        input.nextLine(); // đọc bỏ dòng thừa
+        System.out.println("Bạn có muốn tiếp tục giao dịch? (y/n)");
+        var choice = input.nextLine(); // nhập y/n
+        if (choice.toLowerCase().compareTo("y") == 0) {
+            System.out.println("Nhập số tài khoản nhận tiền: ");
+            var targetAccNumber = input.nextLine();
+            System.out.println("Nhập số tiền muốn chuyển: ");
+            var transferAmount = input.nextInt();
+            if (targetAccNumber.compareTo(acc2.getAccNumber()) == 0) {
+                if (acc.bankTransfer(acc2, transferAmount)) {
+                    System.out.println("==> Chuyển tiền thành công!");
+                } else {
+                    System.out.println("==> Số tiền cần chuyển vượt quá số dư khả dụng." +
+                            "\nChuyển tiền thất bại!");
+                }
+            } else {
+                System.out.println("==> LỖI: Tài khoản nhận tiền không tồn tại.");
+            }
+            System.out.println("============================");
+            showAccInfo(acc);
+            showAccInfo(acc2);
+            // todo: tự bổ sung các thao tác khác nếu muốn
+        } else {
+            System.out.println("==> LỖI: Lựa chọn không hợp lệ!");
+        }
+    }
+
+    private static void showAccInfo(BankAccount account) {
+        DecimalFormat formatter = new DecimalFormat("###,###,###");
+        System.out.println("Số TK: " + account.getAccNumber());
+        System.out.println("Tên TK: " + account.getOwner());
+        System.out.println("Loại TK: " + account.getType());
+        System.out.println("Số dư: " + formatter.format(account.getBalance()));
+        System.out.println("Thời điểm tra cứu: " + new Date());
+        System.out.println("=============================");
     }
 }
